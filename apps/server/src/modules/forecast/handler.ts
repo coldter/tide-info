6295c741a21d91d5763b5046b8da58c54a6a9d36
Kuldeep <hi@kuldeep.tech>
2025-09-09 00:43:46 +0530

    Add caching for location search in forecast handler. Implement cache storage for search and reverse search locations to improve response times and reduce API calls. Update handler logic to utilize cached data when available.

--- apps/server/src/modules/forecast/handler.ts
+++ apps/server/src/modules/forecast/handler.ts
@@ -1,12 +1,32 @@
-import { OpenAPIHono } from "@hono/zod-openapi";
+import { OpenAPIHono, type z } from "@hono/zod-openapi";
 import { HTTPException } from "hono/http-exception";
+import { seconds } from "itty-time";
 import type { Env } from "@/lib/context";
 import { forecastClient } from "@/modules/forecast/forecast-clients";
 import forecastInfoRoutes from "@/modules/forecast/routes";
+import type {
+  reverseSearchLocationResponseSchema,
+  searchLocationResponseSchema,
+} from "@/modules/forecast/schema";
+import { createCachedStorage } from "@/pkg/storage";
 import { defaultHook } from "@/utils/default-hook";
 
 const app = new OpenAPIHono<Env>({ defaultHook });
 
+/**
+ * Cache Storage
+ */
+const cacheStorageSearchLocation = createCachedStorage<
+  z.infer<typeof searchLocationResponseSchema>
+>("search-location", seconds("1 day"));
+
+const cacheStorageReverseSearchLocation = createCachedStorage<
+  z.infer<typeof reverseSearchLocationResponseSchema>
+>("reverse-search-location", seconds("1 day"));
+
+/**
+ * Handler
+ */
 const forecastInfoHandler = app
   .openapi(forecastInfoRoutes.getTideInfo, async (c) => {
     const { lat, lng } = c.req.valid("query");
@@ -20,19 +40,33 @@ const forecastInfoHandler = app
 
   .openapi(forecastInfoRoutes.searchLocation, async (c) => {
     const { q } = c.req.valid("query");
+    const cachedLocations = await cacheStorageSearchLocation.getItem(q);
+    if (cachedLocations) {
+      return c.json(cachedLocations, 200);
+    }
     const locations = await forecastClient.searchLocation(q);
+
+    await cacheStorageSearchLocation.setItem(q, locations);
+
     return c.json(locations, 200);
   })
 
   .openapi(forecastInfoRoutes.reverseSearchLocation, async (c) => {
     const { lat, lng } = c.req.valid("query");
+    const cachedLocation = await cacheStorageReverseSearchLocation.getItem(
+      `${lat}-${lng}`
+    );
+    if (cachedLocation) {
+      return c.json(cachedLocation, 200);
+    }
     const location = await forecastClient.reverseSearchLocation({ lat, lng });
-
     if (!location) {
       throw new HTTPException(404, {
         message: "Location not found",
       });
     }
+
+    await cacheStorageReverseSearchLocation.setItem(`${lat}-${lng}`, location);
 
     return c.json(location, 200);
   });
