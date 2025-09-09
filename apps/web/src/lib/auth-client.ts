326e2c7de5960ab10dcd229996bfdc2aa307d2b1
Kuldeep <hi@kuldeep.tech>
2025-09-09 21:14:45 +0530

    Why do i deploy to vercel !!!!!!!!!!!!!

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,6 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 
+const parsedUrl = new URL(import.meta.env.VITE_SERVER_URL);
+const pathname = parsedUrl.pathname;
 export const authClient = createAuthClient({
-  baseURL: import.meta.env.VITE_SERVER_URL,
-  basePath: "/api/auth",
+  baseURL: parsedUrl.origin,
+  basePath: pathname !== "/" ? `${pathname}/api/auth` : "/api/auth",
 });
